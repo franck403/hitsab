4f90447452d0d37fdfcbd4ba9050ddb6c64972ff
franck403 <79372051+franck403@users.noreply.github.com>
2023-02-04 15:23:05 -0500

    fix little bug

--- tab.js
+++ tab.js
@@ -161,4 +161,4 @@ if ("serviceWorker" in navigator) {
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
-  }
+}
