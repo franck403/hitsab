4780a2a1399bde5d295a0858a5b18e5de443c047
franck403 <79372051+franck403@users.noreply.github.com>
2023-02-04 15:38:37 -0500

    add cache file

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,3 +1,20 @@
+// Files to cache
+const cacheName = 'Hitsab';
+const file = [
+  'index.html',
+  "loading.gif",
+  "navigator.js",
+  "tab.js",
+  "search.svg",
+  "style.css",
+  "manifest.json",
+  "img/home.png",
+  "img/logo.png",
+  "img/logo144.png",
+  "img/multitab.png"
+];
+
+
 // Fetching content using Service Worker
 self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
