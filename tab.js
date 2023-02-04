a0c35526542661861b10810f9c529021602588e0
franck403 <79372051+franck403@users.noreply.github.com>
2023-02-04 15:25:12 -0500

    fix path bug

--- tab.js
+++ tab.js
@@ -157,7 +157,7 @@ fresh()
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
-        .register("sw.js")
+        .register("pwa/sw.js")
         .then(res => console.log("service worker registered"))
         .catch(err => console.log("service worker not registered", err))
     })
