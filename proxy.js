daa50f1b04753df8b565629f384466d30c8a511a
franck403 <79372051+franck403@users.noreply.github.com>
2023-02-08 11:04:09 -0500

    add verfycation to the proxy starter

--- proxy.js
+++ proxy.js
@@ -1,14 +1,21 @@
 // proxy controler
 
 function proxy_start(){
-    console.log("[info] starting the api");
-    fetch("https://proxy6.francoischouin1.repl.co/");
-    console.log("[info] api is started")
-    localStorage.setItem("proxy", "on");
+    var v = localStorage.getItem("proxy");
+    if (v == null) {
+        console.log("[info] starting the api");
+        fetch("https://proxy6.francoischouin1.repl.co/");
+        console.log("[info] api is started")
+        localStorage.setItem("proxy", "on");
+    } 
 }
 
 function proxy_end(){
-    console.log("[info] stoping the proxy");
-    localStorage.setItem("proxy", "stop");
-    console.log("[info] proxy stopped");
+    var v = localStorage.getItem("proxy");
+    if (v == null) {}
+    else {
+        console.log("[info] stoping the proxy");
+        localStorage.setItem("proxy", "stop");
+        console.log("[info] proxy stopped");
+    }
 }
