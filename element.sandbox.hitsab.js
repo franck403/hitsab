266a167c94450207e2d28417953b3cbfed1dd383
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-08 16:06:46 -0400

    sadadas

--- element.sandbox.hitsab.js
+++ element.sandbox.hitsab.js
@@ -1,17 +1,23 @@
 class sandbox extends HTMLElement {
 	constructor() {
-	 super();
-	 var html = this.innerHTML
-	 console.log(html)
-	 var url = this
-	 var get = () => {
-		return fetch("")
-		.then((reponse) => reponse.text())
-		.then((data) => {
-			return data
-		})
-	 }
-	 var website = 
-	 this.innerHTML = website
+		super();
+		var html = this.innerHTML
+		console.log(html)
+		var url = this.src
+		var url = url.replaceAll("https://","https$|")
+		var url = url.replaceAll("http://","http$|")
+		var get = () => {
+			return fetch("https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
+			.then((reponse) => reponse.text())
+			.then((data) => {
+				return data
+			})
+		}
+		var url = this.src
+		var website = get()
+		var urlr = url.replaceAll("https://","https$|")
+		var urlr = url.replaceAll("http://","http$|")
+		var website = website.replaceAll(url,"https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
+		this.innerHTML = website
 	}
 }
