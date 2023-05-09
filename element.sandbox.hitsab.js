8c3b105a00d9dd03f064687d447d26741aeb04dd
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-09 06:54:27 -0400

    sadasd

--- element.sandbox.hitsab.js
+++ element.sandbox.hitsab.js
@@ -2,23 +2,26 @@ class hitsabnavigator extends HTMLElement {
 	constructor() {
 		super();
 		var html = this.innerHTML
-		console.log(html)
-		var url = this.src
-		var url = url.replaceAll("https://","https$|")
-		var url = url.replaceAll("http://","http$|")
-		var get = () => {
-			return fetch("https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
-			.then((reponse) => reponse.text())
-			.then((data) => {
-				return data
-			})
-		}
-		var url = this.src
-		var website = get()
-		var urlr = url.replaceAll("https://","https$|")
-		var urlr = url.replaceAll("http://","http$|")
-		var website = website.replaceAll(url,"https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
-		this.innerHTML = website
+		var element = document.getElementsByTagName("UL")
+
+		element.forEach((element) => {
+			var url = this.src
+			var url = url.replaceAll("https://","https$|")
+			var url = url.replaceAll("http://","http$|")
+			var get = () => {
+				return fetch("https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
+				.then((reponse) => reponse.text())
+				.then((data) => {
+					return data
+				})
+			}
+			var url = this.src
+			var website = get()
+			var urlr = url.replaceAll("https://","https$|")
+			var urlr = url.replaceAll("http://","http$|")
+			var website = website.replaceAll(url,"https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
+			this.innerHTML = website
+		});
 	}
 }
 
