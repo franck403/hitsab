b9a4ca3fe897bd55ee881be9e9593a34e44040b1
franck403 <79372051+franck403@users.noreply.github.com>
2023-05-12 09:04:58 -0400

    adding pwa to the broswer

--- pwa.hitsab.js
+++ pwa.hitsab.js
@@ -1,25 +1,7 @@
 class hitsabapp extends HTMLElement {
 	constructor() {
 		super();
-		var elements = document.getElementsByTagName("hitsab-navigator")
-		for (let i = 0; i < elements.length; i++) {
-			var url = elements[i].attributes.src.value
-			console.log(url)
-			var url = url.replaceAll("https://","https$|")
-			var url = url.replaceAll("http://","http$|")
-			console.log("https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
-			var get = () => {
-				return fetch(url)
-				.then((reponse) => reponse.text())
-				.then((data) => {
-					return data
-				})
-			}
-			var website = await get()
-			console.log(website)
-			var website = website.replaceAll(url,"https://hitsabenvsystems.francoischouin1.repl.co/proxy/" + url)
-			elements[i].innerHTML = website
-		};
+        localapi(this)
 	}
 }
 
